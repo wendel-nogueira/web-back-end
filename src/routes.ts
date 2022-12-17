92e5421d6e39f9f5e5e3f784ed76b28cbaf06613
Wendel Nogueira <43501645+wendel-nogueira@users.noreply.github.com>
2022-12-17 18:55:26 -0300

    feat: Add crud games

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import { UsersController } from './models/users/users.controller';
+import { GamesController } from './models/games/games.controller';
 
 
 const router = Router();
 const usersController = new UsersController();
+const gamesController = new GamesController();
+
 
 //  Users
 router.get('/users', usersController.findAll);
@@ -11,6 +14,14 @@ router.get('/users/:id', usersController.findOne);
 router.post('/users', usersController.create);
 router.patch('/users/:id', usersController.update);
 router.delete('/users/:id', usersController.remove);
+
+//  Games
+router.get('/games', gamesController.findAll);
+router.get('/games/:id', gamesController.findOne);
+router.get('/games/title/:title', gamesController.findByTitle);
+router.post('/games', gamesController.create);
+router.patch('/games/:id', gamesController.update);
+router.delete('/games/:id', gamesController.remove);
 
 
 export { router };
